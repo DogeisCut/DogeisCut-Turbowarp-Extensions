50f43acb8d73866be225696ccdc128b876c17cf1
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2023-09-14 14:55:54 -0600

    lol

--- sprite-parenting.js
+++ sprite-parenting.js
@@ -8,18 +8,50 @@ class SpriteParenting {
             opcode: 'parent_sprite',
             blockType: Scratch.BlockType.COMMAND,
             text: 'parent sprite [ONE] and [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'SPRITE_PARENT_MENU'
+              },
+              TWO: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'SPRITE_PARENT_MENU'
+              }
+            }
           },
           {
             opcode: 'unparent_sprite',
             blockType: Scratch.BlockType.COMMAND,
             text: 'unparent sprite [ONE] and [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'SPRITE_PARENT_MENU'
+              },
+              TWO: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'SPRITE_PARENT_MENU'
+              }
+            }
           },
           {
             opcode: 'unparent_all',
             blockType: Scratch.BlockType.COMMAND,
             text: 'unparent all sprites from [ONE]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'SPRITE_PARENT_MENU'
+              },
+            }
           }
-        ]
+        ],
+        menus: {
+          SPRITE_PARENT_MENU: {
+            acceptReporters: true,
+            items: ['myself', 'insert sprites here']
+          }
+        }
       };
     }
   

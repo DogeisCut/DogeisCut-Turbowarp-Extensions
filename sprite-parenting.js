20100c668b7b3c9509646943a2cd15b3ef695045
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2023-09-14 14:59:46 -0600

    whoops!

--- sprite-parenting.js
+++ sprite-parenting.js
@@ -1,7 +1,7 @@
 class SpriteParenting {
     getInfo() {
       return {
-        id: 'sprite-parenting',
+        id: 'spriteparenting',
         name: 'Sprite Parenting',
         blocks: [
           {
